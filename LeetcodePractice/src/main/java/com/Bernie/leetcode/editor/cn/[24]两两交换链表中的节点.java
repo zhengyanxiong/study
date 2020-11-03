4c7930a311c2ada8c86f05a040cfc74f2b2c728e
zhengyanxiong <742228386@qq.com>
2020-11-03 22:35:16 +0800

    leetcode

---
+++ LeetcodePractice/src/main/java/com/Bernie/leetcode/editor/cn/[24]两两交换链表中的节点.java
@@ -0,0 +1,100 @@
+package com.Bernie.leetcode.editor.cn;
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4]
+//输出：[2,1,4,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 100] 内 
+// 0 <= Node.val <= 100 
+// 
+// Related Topics 链表 
+// 👍 728 👎 0
+
+class SwapNodesInPairs {
+    public static void main(String[] args) {
+        Solution solution = new SwapNodesInPairs().new Solution();
+        ListNode listNode = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4))));
+        ListNode newNode = solution.swapPairs(listNode);
+        System.out.println(newNode.toString());
+
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode swapPairs(ListNode head) {
+            /*if (null == head || null == head.next)
+                return head;
+            ListNode newNode = head.next;
+            head.next = swapPairs(newNode.next);
+            newNode.next = head;
+            return newNode;*/
+            ListNode dummyHead = new ListNode(0);
+            dummyHead.next = head;
+            ListNode temp = dummyHead;
+            while (null != temp.next && null != temp.next.next) {
+                ListNode node1 = temp.next;
+                ListNode node2 = temp.next.next;
+                temp.next = node2;
+                node1.next = node2.next;
+                node2.next = node1;
+                temp = node1;
+            }
+            return dummyHead.next;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
